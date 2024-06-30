f114b60c27985fdab0dee3ca25310e1cd7596214
zafer7255 <97598932+zafer7255@users.noreply.github.com>
2024-07-01 01:32:43 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cly%(5-34f8d1pc_fi*l_p@m3^x%#a$!iq(yu=s&&ez%-_pk$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.109', 'localhost', '127.0.0.1','192.168.170.210']
+ALLOWED_HOSTS = [''] #  write your ip for hosting ex:- ['000.000.0.000', 'localhost', '127.0.0.1']
 
 
 # Application definition
